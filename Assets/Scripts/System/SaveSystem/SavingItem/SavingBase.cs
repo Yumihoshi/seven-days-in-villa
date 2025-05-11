39d718a1b8279ee6aa032ff8dea501771ead3a18
cjrcjrljy <151117441+cjrcjrljy@users.noreply.github.com>
2025-05-11 16:24:29 +0800

    准备尝试保存新建的物体

--- Assets/Scripts/System/SaveSystem/SavingItem/SavingBase.cs
+++ Assets/Scripts/System/SaveSystem/SavingItem/SavingBase.cs
@@ -1,29 +1,32 @@
+using System;
 using Sirenix.OdinInspector;
 using UnityEngine;
-
+/// <summary>
+/// ×Ô¶¯±£´æ
+/// </summary>
 public class SavingBase : MonoBehaviour,RequireSavingItem
 {
     [SerializeField] private string baseKey;
+    
 
-    private void Awake()
+    private void OnApplicationQuit()
     {
-        
+        Save();
     }
 
     private void Start()
     {
         baseKey = SaveSystemManager.Instance.SaveSlotName +cjr.Scence.SceneManager.Instance.GetCurrentScene();
+        Load();
         
     }
-
-    [Button("Save Data")]
+    
     public void Save()
     {
         
         ES3.Save<GameObject>(baseKey, gameObject);
         
     }
-    [Button("Load Data")]
     public void Load()
     {
         if (ES3.KeyExists(baseKey))
