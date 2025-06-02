7835b1693113c6f08611a4a702f322e745b76d1f
cjrcjrljy <151117441+cjrcjrljy@users.noreply.github.com>
2025-06-02 08:38:01 +0800

    [Add] 完善基础功能

--- Assets/Scripts/Player/Hpsystem/PlayerHpSystem.cs
+++ Assets/Scripts/Player/Hpsystem/PlayerHpSystem.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class PlayerHpSystem : MonoBehaviour
 {
-  [SerializeField] Transform HpBar;
+
+  #region UI展示部分
+    [SerializeField] Transform HpBar;
+    [SerializeField] Transform HpHolder;
+    [SerializeField] private GameObject SinGleHp;
+  #endregion
+
+  #region 具体逻辑
+
+    [SerializeField] int HealthPoints;
+
+  #endregion
+
+  [Button("AddHp")]
+  public void AddHp()
+  {
+    HealthPoints++;
+    Instantiate(SinGleHp, HpHolder);
+  }
+  
+  
 }
