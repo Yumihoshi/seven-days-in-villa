027f6385670175f07029c123208143ee2a53ddcf
cjrcjrljy <151117441+cjrcjrljy@users.noreply.github.com>
2025-08-22 10:18:47 +0800

    feat 性别选择

---
+++ Assets/Scripts/Ui/PlayerChoose/PlayerChosenPanel.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChosenPanel : MonoBehaviour
+{
+   public Transform choosing;
+   public Transform chosed;
+
+   public bool isChosen = false;
+   public int choice;
+   private void Start()
+   {
+      choosing.gameObject.SetActive(true);
+      chosed.gameObject.SetActive(false);
+      choice = 0;
+      doChosing(choice);
+   }
+
+   void CleanChildren(Transform holder)
+   {
+      for (int i = 0; i < holder.childCount; i++)
+      {
+         Transform child = holder.GetChild(i);
+         child.gameObject.SetActive(false);
+      }
+   }
+   void doChosing(int choice)
+   {
+      if(isChosen)
+         return;
+      CleanChildren(choosing);
+      CleanChildren(chosed);
+      choosing.GetChild(choice).gameObject.SetActive(true);
+   }
+
+   void dochosed()
+   {
+      CleanChildren(choosing);
+      CleanChildren(chosed);
+      choosing.gameObject.SetActive(false);
+      chosed.gameObject.SetActive(true);
+      chosed.GetChild(choice).gameObject.SetActive(true);
+      isChosen = true;
+   }
+   private void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.A))
+      {
+         choice--;
+      }
+
+      if (Input.GetKeyDown(KeyCode.D))
+      {
+         choice++;
+      }
+
+      if (choice < 0)
+      {
+         choice +=2;
+      }
+      doChosing(choice);
+      choice %= 2;
+      if (Input.GetKeyDown(KeyCode.Return))
+      {
+        dochosed();
+      }
+   }
+}
