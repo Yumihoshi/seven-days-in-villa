721163bf4f81f4ca65e12c79a0f6f690da2cb10b
cjrcjrljy <151117441+cjrcjrljy@users.noreply.github.com>
2025-09-21 18:49:21 +0800

    tmp

---
+++ Assets/Scripts/Rooms/BaseRoom.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yumihoshi.Task;
+
+
+
+[Serializable]
+public enum RoomType
+{
+    
+}
+
+public class BaseRoom : MonoBehaviour
+{
+    public RoomType roomName;
+    
+    
+}
