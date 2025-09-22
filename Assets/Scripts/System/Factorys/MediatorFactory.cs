639cd2bf3cbd30e3a4f80d6bec0d8471d07ff7d9
cjrcjrljy <151117441+cjrcjrljy@users.noreply.github.com>
2025-09-22 15:55:11 +0800

    feat tmp

--- Assets/Scripts/System/Factorys/MediatorFactory.cs
+++ Assets/Scripts/System/Factorys/MediatorFactory.cs
@@ -23,4 +23,21 @@ public class MediatorFactory
         ApplicationFacade.Instance.RegisterMediator(mediator);
         
     }
+    
+    /// <summary>
+    /// 如果指定名字的 Mediator 已注册，则直接移除
+    /// </summary>
+    /// <param name="mediatorName">注册时使用的名字</param>
+    public void UnregisterIfExists(string mediatorName)
+    {
+        if (ApplicationFacade.Instance.HasMediator(mediatorName))
+        {
+            ApplicationFacade.Instance.RemoveMediator(mediatorName);
+            Debug.Log($"[{mediatorName}] 已存在，已取消注册。");
+        }
+        else
+        {
+            Debug.Log($"[{mediatorName}] 未注册，无需取消。");
+        }
+    }
 }
